ea9deca26cd471aa77c54bdb79964bea9a2f52b1
mahiboobnadaf <rasool97260@gmail.com>
2024-10-23 17:49:38 +0530

    nested dynamic routes

--- routes-learning/src/app/products/[productId]/page.jsx
+++ routes-learning/src/app/products/[productId]/page.jsx
@@ -1,5 +1,9 @@
 export default function Product_Details({params}){
     
+    for (const key in params) {
+        console.log(key + "---------------")
+    }
+    
     return (
         <h1> Product Details {params.productId}  
         </h1>
---
+++ routes-learning/src/app/products/[productId]/reviews/[reviewsId]/page.jsx
@@ -0,0 +1,8 @@
+export default function Reviews_Details({params}){
+    console.log(params)
+    return (
+        <>
+            <h1> Reviews Details {params.reviewsId} </h1>
+        </>
+    )
+}

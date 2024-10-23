59140b44f15afea2bf0e8d11da344e8906647c90
mahiboobnadaf <rasool97260@gmail.com>
2024-10-23 14:02:00 +0530

    Routing Example

---
+++ routes-learning/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div className="font-bold underline text-3x h-screen flex justify-center text-red-500 p-10">
+            <p>Welcome to Home Page!</p> 
+        </div>
+    )
+}
---
+++ routes-learning/src/app/profile/friend_profile/page.jsx
@@ -0,0 +1,7 @@
+export default function Friend_profile(){
+    return (
+        <div>
+            <h1>Friends Profile Page</h1>
+        </div>
+    )
+}

0371ff0fc57706112c0404f10155d0138c2c69ab
mahiboobnadaf <rasool97260@gmail.com>
2024-10-23 17:37:21 +0530

    Dynamic Route example Product

---
+++ routes-learning/src/app/products/[productId]/page.jsx
@@ -0,0 +1,7 @@
+export default function Product_Details({params}){
+    
+    return (
+        <h1> Product Details {params.productId}  
+        </h1>
+    )
+}
---
+++ routes-learning/src/app/products/page.jsx
@@ -0,0 +1,15 @@
+export default function Products(){
+    return (
+       <>
+        <h1>Products are</h1>
+        <h3>
+            <ul>
+                <li>Product 1</li>
+                <li>Product 2</li>
+                <li>Product 3</li>
+            </ul>
+        </h3>
+       </> 
+    )
+
+}

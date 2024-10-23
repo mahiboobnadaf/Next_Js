775d9f7db2099a51e54950fddaab5a2c70cd5db1
mahiboobnadaf <rasool97260@gmail.com>
2024-10-23 13:31:46 +0530

    Welcome Msg

---
+++ my-hello-app/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-serif font-extrabold w-screen h-screen text-3xl p-2 flex items-center justify-center">
+      <p>WelCome to Next js : Mahiboob Nadaf</p>
+    </div>
+  );
+}

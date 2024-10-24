6620d5f6a35ea5821038b0f2c781c86377b344d9
mahiboobnadaf <rasool97260@gmail.com>
2024-10-24 19:35:10 +0530

    tailwindcss working

--- routes-learning/src/app/page.js
+++ routes-learning/src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home(){
     return (
-        <div className="font-bold underline text-3x h-screen flex justify-center text-red-500 p-10">
+        <div className="font-extrabold underline text-3xl h-96 flex items-center justify-center text-red-700">
             <p>Welcome to Home Page!</p> 
         </div>
     )

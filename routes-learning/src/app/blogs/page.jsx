60ee2df7c9a2327ada48c9ad857373c2822a61d9
mahiboobnadaf <rasool97260@gmail.com>
2024-10-25 08:54:24 +0530

    Link tag href

--- routes-learning/src/app/blogs/page.jsx
+++ routes-learning/src/app/blogs/page.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Blog(){
     return (
-        <div>
-            <h1>Blog Page</h1>
-        </div>
+        <>
+            <div>
+                <h1>Blog Page</h1>
+            </div>
+            <Link href='/' className="flex flex-col items-center bg-black text-blue-500 underline "> Home </Link>
+        </>
+
     )
 }

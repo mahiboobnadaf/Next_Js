2e099d94df6dc4c4a79587195164da240748c8a3
mahiboobnadaf <rasool97260@gmail.com>
2024-10-25 09:15:03 +0530

    Link tag replace attribute

--- routes-learning/src/app/blogs/first_blog/page.jsx
+++ routes-learning/src/app/blogs/first_blog/page.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function First_Blog(){
     return (
         <div>
             <h1>1st Blog Page</h1>
+            <Link href='/blogs/second_blog' replace>blogs</Link>
         </div>
     )
 }
--- routes-learning/src/app/blogs/page.jsx
+++ routes-learning/src/app/blogs/page.jsx
@@ -6,7 +6,11 @@ export default function Blog(){
             <div>
                 <h1>Blog Page</h1>
             </div>
-            <Link href='/' className="flex flex-col items-center bg-black text-blue-500 underline "> Home </Link>
+            <div className="flex flex-col items-center bg-black text-blue-500 underline ">
+                <Link href="/blogs/first_blog"> 1st blog </Link>
+                <Link href="/blogs/second_blog" replace>  2nd blog  </Link>
+                <Link href='/' className="flex flex-col items-center bg-black text-blue-500 underline "> Home </Link>
+            </div>
         </>
 
     )

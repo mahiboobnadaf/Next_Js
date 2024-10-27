ad1298e783bb20fba2feab86e784a3c49e14cb36
mahiboobnadaf <rasool97260@gmail.com>
2024-10-27 23:23:12 +0530

    Card added

--- routes-learning/src/app/dashboard/layout.jsx
+++ routes-learning/src/app/dashboard/layout.jsx
@@ -1,10 +1,12 @@
+import Card from "../components/card"
+
 export default function Layout({ children ,user,revenue, notifications}) {
   return (
     <>
         <div> {children} </div>
-        <div> {user} </div>
-        <div> {revenue} </div>
-        <div> {notifications} </div>
+        <Card> {user} </Card>   
+        <Card> {revenue} </Card>        
+        <Card> {notifications} </Card>
     </>
   )
 }

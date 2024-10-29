86213076c2861ce5ad0e3a2c81509c2e3f462248
mahiboobnadaf <rasool97260@gmail.com>
2024-10-29 13:13:41 +0530

    Login page

--- routes-learning/src/app/dashboard/layout.jsx
+++ routes-learning/src/app/dashboard/layout.jsx
@@ -1,7 +1,8 @@
 import Card from "../components/Card"
 
-export default function Layout({ children ,user,revenue, notifications}) {
-  return (
+export default function Layout({ children ,user,revenue, notifications, login}) {
+  const isLogin = false
+  return isLogin ? ( 
     <>
         <div className="p-10"> {children} </div>
         <div className="h-[600px] items-center flex justify-center font-extrabold bg-slate-400"> 
@@ -14,5 +15,5 @@ export default function Layout({ children ,user,revenue, notifications}) {
             </div>
         </div>    
     </>
-  )
+  ) : (login)
 }

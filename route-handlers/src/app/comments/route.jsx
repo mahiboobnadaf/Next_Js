c00a7fb1556584fee6488ce3b3442ff442e62abf
mahiboobnadaf <rasool97260@gmail.com>
2024-10-30 16:04:03 +0530

    post request working

--- route-handlers/src/app/comments/route.jsx
+++ route-handlers/src/app/comments/route.jsx
@@ -5,16 +5,23 @@ export async function GET(){
     return Response.json(comments)
 }
 
-export async function POST(req){
+export async function POST(req) {
     const comment = await req.json();
 
     const newComment = {
-        id : comments.length + 1,
-        text : comment.text
-    }
+        id: comments.length + 1,
+        text: comment.text
+    };
 
-    // console.log(newComment)
+    comments.push(newComment);
 
-    comments.push(newComment)
-    return new Response(JSON.stringify(newComment), req.headers)
+    return new Response(
+        JSON.stringify(newComment), 
+        { 
+            headers: { 
+                'Content-Type': 'application/json' 
+            },
+            status : 201 
+        }
+    );
 }

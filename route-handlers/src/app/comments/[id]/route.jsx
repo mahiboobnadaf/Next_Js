b56bd5c580ad353757814dabf10a0ae7aec3012a
mahiboobnadaf <rasool97260@gmail.com>
2024-10-31 14:43:37 +0530

    .

--- route-handlers/src/app/comments/[id]/route.jsx
+++ route-handlers/src/app/comments/[id]/route.jsx
@@ -1,6 +1,13 @@
 import { comments } from "../data"
+
 export async function GET(_req,{params}){
 
     const comment =comments.find(async (comment) => comment.id === parseInt(await params.id))
+    return Response.json(comment)
+}
+
+export async function PATCH(req,{params}){
+
+    const comment =comments.findIndex(async (comment) => comment.id === parseInt(await params.id))
     return Response.json(comment)
 }
